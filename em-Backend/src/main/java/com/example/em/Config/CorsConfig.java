a436e6769ae4e1b451de4603853a229b5f62ac5c
JohnCh-867 <johnandersonchaponanm@gmail.com>
2026-07-12 20:12:31 -0500

    Update CorsConfig.java

--- em-Backend/src/main/java/com/example/em/Config/CorsConfig.java
+++ em-Backend/src/main/java/com/example/em/Config/CorsConfig.java
@@ -16,7 +16,8 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("http://localhost:4200",
+                "https://sistemaempelados.jacm.pro"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
         config.setAllowCredentials(true);
